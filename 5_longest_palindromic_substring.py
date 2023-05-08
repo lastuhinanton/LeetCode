c19c7fb9005d8db00f7481027060feaefd388364
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-08 10:08:02 +0300

    second_version

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -1,23 +1,15 @@
 pallindrome = "cbbd"
 
-def check_pallindrome(start, end, string):
-    while start < end:
-        if string[start] != string[end]: return False
-        start += 1
-        end -= 1
-    return True
-
-def check_longest(prime, i, j):
-    if (prime[1] - prime[0]) < (j - i):
-        prime[0], prime[1] = i, j
-
 def longestPalindrome(s) -> str:
     longest = [0, 0]
-    for i in range(len(s)):
-        j = len(s)-1
+    size_s = len(s)
+    for i in range(size_s):
+        j = size_s-1
         while i < j:
-            if check_pallindrome(i, j, s):
-                check_longest(longest, i, j)
+            tmp = s[i:j+1]
+            if tmp[::-1] == tmp:
+                if (longest[1] - longest[0]) < (j - i):
+                    longest[0], longest[1] = i, j
                 j = -1
             j -= 1
     return s[longest[0]:longest[1]+1]
